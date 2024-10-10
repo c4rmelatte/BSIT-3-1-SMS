eea560a3b96ac23525ddec5cec83cac299ec873d
Mimician <santosczianel@gmail.com>
2024-10-10 22:16:58 +0800

    update on employee, position, and role tables DB

---
+++ app/Http/Controllers/PositionsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Positions;
+use App\Http\Requests\StorePositionsRequest;
+use App\Http\Requests\UpdatePositionsRequest;
+
+class PositionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePositionsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Positions $positions)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Positions $positions)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePositionsRequest $request, Positions $positions)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Positions $positions)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+Route::apiResource('employees', EmployeesController::class);
+Route::apiResource('positions', PositionsController::class);
+Route::apiResource('roles', RolesController::class);
