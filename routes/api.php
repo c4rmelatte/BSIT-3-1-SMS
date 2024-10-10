20e1e1ee45ef9f27fd5a5feb1931583ffc7c8c2a
Mimician <santosczianel@gmail.com>
2024-10-10 22:26:48 +0800

    added bugs in API, utos po ni michael

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeesController;
+use App\Http\Controllers\PositionsController;
+use App\Http\Controllers\RolesController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
