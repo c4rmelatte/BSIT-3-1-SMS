0dccf1eb8a33cd3ae9eec10c55a4489e6ff036be
Kryoz0512 <markorbertbayudang5@gmail.com>
2024-10-25 14:48:49 +0800

    student palang may dashboard d pa yari dahil la pa ung database

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -7,24 +7,20 @@
 
     <!-- CSS bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<style>
+    body {
+    margin: 0;
+    padding: 0;
+    display: flex;
+}
+</style>
 </head>
+
 <body>
-<header> 
-    <!-- navbar here -->
-</header>
-
-
-<main>
-    <!--body of chr-->
-</main>
-
-<footer>
-    <!--malamang footer-->
-</footer>
-
-
-<!-- script -->
+    <div class="container">
+        @yield('content')
+    </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentDashboardController;
+use App\Http\Controllers\AdminDashboardController;
+use App\Http\Controllers\TeacherDashboardController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+
+Route::get('/studentdash', [StudentDashboardController::class, 'index']);
+Route::get('/admindash', [AdminDashboardController::class, 'index']);
+Route::get('/teacherdash', [TeacherDashboardController::class, 'index']);
